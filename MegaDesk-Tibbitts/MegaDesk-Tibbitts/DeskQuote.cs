6c68ffbefe701b075f421b1fb4bec7cd1c3cce9c
DAREN TIBBITTS <tomtomm@byui.edu>
2022-10-13 19:48:26 -0600

    getRushOrder function

--- MegaDesk-Tibbitts/MegaDesk-Tibbitts/DeskQuote.cs
+++ MegaDesk-Tibbitts/MegaDesk-Tibbitts/DeskQuote.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Newtonsoft.Json.Linq;
 
 namespace MegaDesk_Tibbitts
 {
@@ -20,6 +22,11 @@ namespace MegaDesk_Tibbitts
         public int rushCost;
         public int materialCost;
         public int drawerCost;
+        public int rows = 3;
+        public int columns = 3;
+
+        // Multi dimensional array variable.
+        public int[,] RushArray;
         public static int quoteNum { get; set; }
 
         public DeskQuote()
@@ -69,15 +76,32 @@ namespace MegaDesk_Tibbitts
                     materialCost += 0;
                     break;
             }
-            
+
             return materialCost;
         }
-        /*
-        public void getRushOrder()
-        {
 
+        public void getRushOrder(out int[,] array, int rows, int columns)
+        {
+            // Read from file.
+            string path = @"..\rushOrderPrices.txt";
+
+            // Read All Lines Method.
+            string[] readText = File.ReadAllLines(path);
+            array = new int[rows, columns];
+            int index = 0;
+
+            // Create multi dimensional array.
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    int num = Int32.Parse(readText[index]);
+                    array[i, j] = num;
+                    index++;
+                }
+            }
         }
-        */
+
 
         public int getRushCost(int rush)
         {
@@ -85,29 +109,29 @@ namespace MegaDesk_Tibbitts
             if (linearFeet < 1000)
             {
                 if (rush == 7)
-                    rushCost = 30;
+                    rushCost = RushArray[2, 0];
                 else if (rush == 5)
-                    rushCost = 40;
+                    rushCost = RushArray[1, 0];
                 else if (rush == 3)
-                    rushCost = 60;
+                    rushCost = RushArray[0, 0];
             }
             else if (linearFeet >= 1000 && linearFeet < 2000)
             {
                 if (rush == 7)
-                    rushCost = 35;
+                    rushCost = RushArray[2, 1];
                 else if (rush == 5)
-                    rushCost = 50;
+                    rushCost = RushArray[1, 1];
                 else if (rush == 3)
-                    rushCost = 70;
+                    rushCost = RushArray[0, 1];
             }
             else if (linearFeet >= 2000)
             {
                 if (rush == 7)
-                    rushCost = 40;
+                    rushCost = RushArray[2, 2];
                 else if (rush == 5)
-                    rushCost = 60;
+                    rushCost = RushArray[1, 2];
                 else if (rush == 3)
-                    rushCost = 80;
+                    rushCost = RushArray[0, 2];
             }
             else
             {
@@ -119,6 +143,7 @@ namespace MegaDesk_Tibbitts
 
         public int getTotal(Desk Desk)
         {
+            getRushOrder(out RushArray, rows, columns);
             int baseCost = 200;
             linearFeet = getLinearFeetCost();
             drawerCost = getDrawerCost();
@@ -129,3 +154,4 @@ namespace MegaDesk_Tibbitts
         }
     }
 }
+
